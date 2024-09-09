352f27dcc14846d9276089993f49555d809f2869
Kacper Koczot <kacperkocz@gmail.com>
2024-09-09 20:53:35 +0200

    Add arrow svg and make rightsidebar work better and look better

---
+++ components/shared/UserCarousel.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import HandleFollowBtn from "../interactiveElements/HandleFollowBtn";
+
+function UserCarousel({ users, userId, follow }: { users: any[], userId: string, follow: "follow" | "unfollow" }) {
+  const [current, setCurrent] = useState(1);
+  if (!users.length) return (
+    <p className="text-white text-center">No users to display</p>
+  )
+  return (
+    <div className="flex gap-2">
+      <button
+        className={`text-white text-[24px] rounded-sm hover:bg-green-500/40 disabled:bg-green-700/15 ${current <= 1 ? "bg-green-700/15" : "bg-green-700/30"}`}
+        disabled={current <= 1}
+        onClick={() => setCurrent(prev => prev - 1)}
+      >
+        <Image src="/assets/left-arrow.svg" alt="show previous" width={12} height={12} />
+      </button>
+      <div key={users[current - 1]?.id} className="flex flex-col items-center flex-1">
+        <Link href={`/profile/${users[current - 1]?.id}`}>
+          <Image className="rounded-full block" src={users[current - 1]?.image} alt={users[current - 1]?.username} width={52} height={52} />
+        </Link>
+        <p className="text-white mt-2 mb-1">@{users[current - 1]?.username}</p>
+        <span className="p-[2px] border border-transparent rounded-lg hover:border-white">
+          <HandleFollowBtn authUserId={userId} userId={users[current - 1]?.id} follow={follow} />
+        </span>
+      </div>
+      <button
+        className={`text-white text-[24px] rounded-sm hover:bg-green-500/40 disabled:bg-green-700/15 ${current >= users.length ? "bg-green-700/15" : "bg-green-700/30"}`}
+        disabled={current >= users.length}
+        onClick={() => setCurrent(prev => prev + 1)}
+      >
+        <Image src="/assets/left-arrow.svg" alt="show next" width={12} height={12} className="rotate-180" />
+      </button>
+    </div>
+  );
+}
+
+export default UserCarousel;
--- components/shared/mainLayout/RightSidebar.tsx
+++ components/shared/mainLayout/RightSidebar.tsx
@@ -1,55 +1,58 @@
 import HandleFollowBtn from "@/components/interactiveElements/HandleFollowBtn";
-import { fetchFollowedAuthors, fetchSimilarAuthors } from "@/lib/actions/user.actions";
+import { fetchFollowedAuthors, suggestedAuthors } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import UserCarousel from "../UserCarousel";
 
 async function RightSidebar() {
   const user = await currentUser();
   if (!user) return null;
-  const similarAuthors = await fetchSimilarAuthors(user.id) || [];
+  const similarAuthors = await suggestedAuthors(user.id, "similar") || [];
   const followedAuthors = await fetchFollowedAuthors(user.id) || [];
   // console.log("user: ", user);
   return (
     <section className="custom-scrollbar rightsidebar">
       <div className="felx flex-1 felx-col justify-start">
-        <h3 className="text-heading4-medium text-light-1">Authors for you:</h3>
+        <h3 className="text-heading4-medium text-light-1 mb-3">Authors for you:</h3>
         <div className="flex flex-col gap-4 divide-y-2 border-b-2 pb-4 border-white">
           {
-            similarAuthors?.map(author => {
-              return (
-                <div key={author.id} className="flex flex-col items-center">
-                  <Link href={`/profile/${author.id}`}>
-                    <Image className="rounded-full block" src={author.image} alt={author.username} width={64} height={64} />
-                  </Link>
-                  <p className="text-white mt-2 mb-1">@{author.username}</p>
-                  <span className="hover:bg-dark-1 p-[2px] rounded-lg">
-                  <HandleFollowBtn authUserId={user.id} userId={author.id} follow="follow" />
-                  </span>
-                </div>
-              )
-            })
+            // similarAuthors?.map((author: any) => {
+            //   return (
+            //     <div key={author.id} className="flex flex-col items-center">
+            //       <Link href={`/profile/${author.id}`}>
+            //         <Image className="rounded-full block" src={author.image} alt={author.username} width={64} height={64} />
+            //       </Link>
+            //       <p className="text-white mt-2 mb-1">@{author.username}</p>
+            //       <span className="hover:bg-dark-1 p-[2px] rounded-lg">
+            //       <HandleFollowBtn authUserId={user.id} userId={author.id} follow="follow" />
+            //       </span>
+            //     </div>
+            //   )
+            // })
           }
+          <UserCarousel users={JSON.parse(JSON.stringify(similarAuthors))} userId={user.id} follow="follow" />
         </div>
       </div>
       <div className="felx flex-1 felx-col justify-start">
-        <h3 className="text-heading4-medium text-light-1">Followed Authors:</h3>
+        <h3 className="text-heading4-medium text-light-1 mb-3">Followed Authors:</h3>
         <div className="flex flex-col gap-4 divide-y-2 border-b-2 pb-4 border-white">
           {
-            followedAuthors?.map(author => {
-              return (
-                <div key={author.id} className="flex flex-col items-center">
-                  <Link href={`/profile/${author.id}`}>
-                    <Image className="rounded-full block" src={author.image} alt={author.username} width={64} height={64} />
-                  </Link>
-                  <p className="text-white mt-2 mb-1">@{author.username}</p>
-                  <span className="hover:bg-dark-1 p-[2px] rounded-lg">
-                  <HandleFollowBtn authUserId={user.id} userId={author.id} follow="unfollow" />
-                  </span>
-                </div>
-              )
-            })
+            // followedAuthors?.map(author => {
+            //   return (
+            //     <div key={author.id} className="flex flex-col items-center">
+            //       <Link href={`/profile/${author.id}`}>
+            //         <Image className="rounded-full block" src={author.image} alt={author.username} width={64} height={64} />
+            //       </Link>
+            //       <p className="text-white mt-2 mb-1">@{author.username}</p>
+            //       <span className="hover:bg-dark-1 p-[2px] rounded-lg">
+            //       <HandleFollowBtn authUserId={user.id} userId={author.id} follow="unfollow" />
+            //       </span>
+            //     </div>
+            //   )
+            // })
           }
+          <UserCarousel users={JSON.parse(JSON.stringify(followedAuthors))} userId={user.id} follow="unfollow" />
         </div>
       </div>
     </section>
