35b6a6442b51e5536a5899395843e9e5b74e57b3
Kacper Koczot <kacper04business@gmail.com>
2024-11-03 22:56:57 +0100

    Sign-up element centered,create/edit  poem content up to 2500 chars

--- lib/validations/poem.ts
+++ lib/validations/poem.ts
@@ -3,13 +3,13 @@ import * as z from 'zod';
 export const PoemValidation = z.object({
     title: z.string().min(3, {message: "Minimum 3 characters"}).max(50, {message: "Maximum 50 characters"}),
     type: z.string().min(3, {message: "You must choose a type for your poem!"}),
-    content: z.string().min(3, {message: "Minimum 3 characters"}).max(1600, {message: "Maximum 1600 characters"}),
+    content: z.string().min(3, {message: "Minimum 3 characters"}).max(2500, {message: "Maximum 2500 characters"}),
 });
 
 export const EditPoemValidation = z.object({
     title: z.string().min(3, {message: "Minimum 3 characters"}).max(50, {message: "Maximum 50 characters"}),
     type: z.string().optional(),
-    content: z.string().min(3, {message: "Minimum 3 characters"}).max(1600, {message: "Maximum 1600 characters"}),
+    content: z.string().min(3, {message: "Minimum 3 characters"}).max(2500, {message: "Maximum 2500 characters"}),
     folderDest: z.string().optional(),
 })
 
