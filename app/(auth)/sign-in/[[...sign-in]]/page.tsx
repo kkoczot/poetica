4e00d4aeb1ca8529edc2beb4358a8d8c854cbeed
Kacper Koczot <kacper04business@gmail.com>
2025-02-18 23:32:51 +0100

    Add button to go back to home in sign-in/up pages

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,13 @@
 import { SignIn } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Page() {
-    return <div className="flex items-center justify-center min-h-screen"><SignIn /></div>;
+  return (<div className="flex flex-col items-center justify-center min-h-screen py-8">
+    <div className="flex flex-col gap-4">
+      <SignIn />
+      <Link href="/" className="inline-flex min-w-max mx-5 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 text-primary-foreground hover:bg-green-700/50 h-12 px-4 py-2 bg-green-700">
+        Back to home page
+      </Link>
+    </div>
+  </div>);
 }
